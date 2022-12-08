1d2d84de79c529b8b98551b5542a80a15e37173f
Prince Narteh <prinarteh@gmail.com>
2022-12-08 02:55:01 +0000

    add Post model

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def get_posts():
     return {"message": "Welcome to my api"}
 
 @app.post("/")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"posts": "Returns all posts"}
+def create_post(post: Post):
+    print()
+    return {"posts": post}
